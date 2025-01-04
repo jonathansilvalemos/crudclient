884853d8983095c7a8abaf5db40316de211c1e23
Jonathan Lemos <jonathan.lemos@aluno.riogrande.ifrs.edu.br>
2025-01-04 01:54:56 -0300

    GET clients paged

--- src/main/java/com/jsl/crud/services/ClientService.java
+++ src/main/java/com/jsl/crud/services/ClientService.java
@@ -4,9 +4,12 @@ import com.jsl.crud.dto.ClientDTO;
 import com.jsl.crud.entities.Client;
 import com.jsl.crud.repositories.ClientRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.List;
 import java.util.Optional;
 
 @Service
@@ -18,5 +21,11 @@ public class ClientService {
     public ClientDTO findById(Long id){
         Client client = repository.findById(id).get();
         return new ClientDTO(client);
+    }
+
+    @Transactional(readOnly = true)
+    public Page<ClientDTO> findAll(Pageable pageable){
+        Page<Client> result = repository.findAll(pageable);
+        return result.map(x -> new ClientDTO(x));
     }
 }
